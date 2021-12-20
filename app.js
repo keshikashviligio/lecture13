7b8075e061eb7950ed7d1a05192f6bc2339c487c
Giorgi Keshikashvili <keshikashviligio@gmail.com>
2021-12-21 00:31:10 +0400

    Init

---
+++ app.js
@@ -0,0 +1,27 @@
+// var name = 'Gio';
+// let name1 = 'Gio1';
+// const name1 = 'Gio2';
+
+const myString = 'My String value';
+const myNumber = 30;
+const myBoolean = true;
+const myBoolean1 = false;
+const myNull = null;
+const myUndefined = undefined;
+
+// console.log('myString', typeof myString);
+// console.log('myNumber', typeof myNumber);
+// console.log('myBoolean', typeof myBoolean);
+// console.log('myBoolean1', typeof myBoolean1);
+// console.log('myNull', typeof myNull);
+// console.log('myUndefined', typeof myUndefined);
+
+// String concatenation
+
+const myName = 'Giorgi';
+const myAge = 32;
+const amIAdult = true;
+
+// console.log(amIAdult);
+// console.log('My name is: ' + myName + ' and my age is: ' + myAge + 'and I am an: ' + amIAdult);
+console.log(`My name is: ${myName} and my age is: ${myAge} and I am an: ${amIAdult}`);
